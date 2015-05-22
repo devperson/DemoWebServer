6df0cb083a6c49c68c9aaf2c543913003173364b
Khasan <xasan2006@gmail.com>
2015-05-22 21:07:14 +0500

    db initializer change

--- ASPMvcApplication1/Models/DataAccess/DataBaseInitializer.cs
+++ ASPMvcApplication1/Models/DataAccess/DataBaseInitializer.cs
@@ -11,7 +11,7 @@ namespace HServer.Models.DataAccess
     /// This class used before to make connection to DataBase, 
     /// here we can make configurations such as CreateDatabaseIfNotExists, DropCreateDatabaseAlways, DropCreateDatabaseIfModelChanges
     /// </summary>
-    public class DataBaseInitializer : DropCreateDatabaseAlways<DataBaseContext>
+    public class DataBaseInitializer : CreateDatabaseIfNotExists<DataBaseContext>
     {
 
         /// <summary>
