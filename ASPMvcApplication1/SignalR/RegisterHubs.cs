fa2f6666e86bbe1c2744a6f1680753b8b358178e
Khasan <xasan2006@gmail.com>
2015-05-30 17:57:04 +0500

    fix for hub reg

--- ASPMvcApplication1/SignalR/RegisterHubs.cs
+++ ASPMvcApplication1/SignalR/RegisterHubs.cs
@@ -1,16 +1,27 @@
 using System.Web;
 using System.Web.Routing;
 using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
 
-[assembly: PreApplicationStartMethod(typeof(ASPMvcApplication1.RegisterHubs), "Start")]
+//[assembly: PreApplicationStartMethod(typeof(ASPMvcApplication1.RegisterHubs), "Start")]
 
+[assembly: OwinStartup(typeof(ASPMvcApplication1.RegisterHubs))]
 namespace ASPMvcApplication1
 {
-    public static class RegisterHubs
+    //public static class RegisterHubs
+    //{
+    //    public static void Start()
+    //    {            
+    //        RouteTable.Routes.MapHubs(new HubConfiguration());
+    //    }
+    //}
+
+    public class RegisterHubs
     {
-        public static void Start()
-        {            
-            RouteTable.Routes.MapHubs(new HubConfiguration());
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
         }
     }
 }
